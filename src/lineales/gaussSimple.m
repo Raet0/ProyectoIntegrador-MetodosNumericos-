10d3157dce77267495561b6b39fc558db788d28b
raet0 <yosoyrafaelprieto@gmail.com>
2026-01-17 20:07:20 -0500

    primer commit

---
+++ src/lineales/gaussSimple.m
@@ -0,0 +1,16 @@
+function [outputArg1,outputArg2] = gaussSimple(inputArg1,inputArg2)
+%GAUSSSIMPLE Summary of this function goes here
+%   Detailed explanation goes here
+arguments (Input)
+    inputArg1
+    inputArg2
+end
+
+arguments (Output)
+    outputArg1
+    outputArg2
+end
+
+outputArg1 = inputArg1;
+outputArg2 = inputArg2;
+end
